e2e8928106fe35d2f3f7d4f1614cbb06c802fa9f
Ashuakhira <bashok592@gmail.com>
2023-03-18 12:12:12 +0530

    added line computation uc1 file

---
+++ Linecomputation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+            //LineDifference.LineDistance();
+            //CheckLineEquality.LineEquality();
+            CompareTwoLines.CompareLines();
+        }
+    }
+}
